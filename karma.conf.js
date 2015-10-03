093a2374f0872f3ea7e20b93c5e9337312f0853d
Sue Zheng Hao <szhlibrary@gmail.com>
2015-10-03 16:28:12 +0800

    Bower: Add angular-material and angular-nlp-compromise

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,13 @@ module.exports = function(config){
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-animate/angular-animate.js',
+      'app/bower_components/angular-aria/angular-aria.js',
+      'app/bower_components/angular-material/angular-material.js',
+
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-material/angular-material-mocks.js',
+
       'app/components/**/*.js',
       'app/view*/**/*.js'
     ],
