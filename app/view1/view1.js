6a24eb97bc405a89df939c6f030225494ca0ad31
Sue Zheng Hao <szhlibrary@gmail.com>
2015-10-06 00:43:19 +0800

    CV Parsing: WIP code for getting text from a PDF.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,10 +10,6 @@ angular.module('myApp.view1', ['ngRoute', 'ngFileUpload'])
 }])
 
 .controller('View1Ctrl', function($scope, fileReader) {
-  $scope.$watch('files', function () {
-    $scope.upload($scope.files);
-  });
-
   $scope.$watch('file', function () {
     if ($scope.file != null) {
       $scope.files = [$scope.file];
@@ -24,15 +20,33 @@ angular.module('myApp.view1', ['ngRoute', 'ngFileUpload'])
     $scope.progress = (progress.loaded / progress.total) * 100;
   });
 
-  $scope.upload = function (files) {
+  $scope.processFiles = function (files) {
     if (files && files.length) {
       for (var i = 0; i < files.length; i++) {
+        console.log(files[i]);
         $scope.showProgressBar = true;
         fileReader.readAsDataUrl(files[i], $scope)
           .then(function(result) {
             console.log(result);
+            PDFJS.getDocument(result).then(function(pdf) {
+              console.log(pdf);
+              pdf.getPage(1).then(function(page) { //FIXME: temporary hardcode pg1 for testing
+                page.getTextContent().then(function(textContent) {
+                  // TODO: Figure out how to chain promises properly, this is ugly as heck
+                  var strings = textContent.items.map(function (item) {
+                    return item.str;
+                  });
+                  console.log('## Text Content ##');
+                  console.log(strings.join(' '));
+                })
+              })
+            });
           });
       }
     }
-  }
+  };
+
+  $scope.doProcess = function () {
+    $scope.processFiles($scope.files);
+  };
 });
