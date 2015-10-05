505d75d44de8f3c4d91c64b5aa4130744f90bbd9
Sue Zheng Hao <szhlibrary@gmail.com>
2015-10-05 15:44:42 +0800

    Added ng-file-upload

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngFileUpload'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
