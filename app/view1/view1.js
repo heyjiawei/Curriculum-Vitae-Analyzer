0c9b07b172b37ff0850af7a67863dde4c14261df
Sue Zheng Hao <szhlibrary@gmail.com>
2015-10-13 17:52:54 +0800

    Accidentally murdered a js file

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,3 +1,14 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'ngFileUpload'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+
 .controller('View1Ctrl', function($scope, fileReader, pdfReader, lemma, cvTokenizer) {
     $scope.page1content = "No file opened.";
     $scope.fileNames = "";
