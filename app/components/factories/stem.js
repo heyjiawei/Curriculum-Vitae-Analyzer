07f30d83f5396dc2649092b0f41ae667499b984e
Peining <peining_pan@hotmail.com>
2015-11-07 21:25:29 +0800

    add stemmer again

---
+++ app/components/factories/stem.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp.factories')
+
+    .factory('stem', function($q) {
+        var stemArray = function(words) {
+            return words.map(function(word) {
+                return stemmer(word);
+            });
+        };
+        return {
+            /**
+             * @param: array of strings
+             * @return: array of stemmed strings
+             */
+            stem_array: stemArray
+        };
+    });
