5441d1f458d63e663102b452faca9139746aa0d0
Sue Zheng Hao <szhlibrary@gmail.com>
2015-11-08 11:12:04 +0800

    View1 Controller: remove routing config since it's already in app.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,16 +1,7 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute', 'ngFileUpload'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {
-      templateUrl: 'view1/view1.html',
-      controller: 'View1Ctrl'
-    })
-    .when('/view2', {
-      templateUrl: 'view2/view2.html',
-      controller: 'View2Ctrl'
-    });
-  }])
+
 
 .controller('View1Ctrl', function($scope, $q, fileReader, pdfReader, cvModel, jobDescriptionModel, cvEvaluator, $location) {
     $scope.fileNames = "";
